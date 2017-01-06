aefa1217b143f9f86898a790013991946caa9f3f
Daniel <danielstuessy@gmail.com>
2017-01-06 09:37:07 -0500

    Parse files into fileModules for linting

--- mcsslint.js
+++ mcsslint.js
@@ -1,11 +1,47 @@
+const fs = require('fs');
 const css = require('css');
 
 module.exports = (function () {
 
     const Linter = {};
 
-    Linter.lint = function() {
+    function isUnique(filePath, i, list) {
+        return list.indexOf(filePath) === i;
+    }
 
+    function getFilePaths(dirPath) {
+        let entryNames = fs.readdirSync(dirPath);
+        let entryPaths = entryNames.map(entryName => `${dirPath}/${entryName}`);
+        let filePaths = entryPaths.filter(entryPath => /\.css$/.test(entryPath));
+        let dirPaths = entryPaths.filter(entryPath => fs.lstatSync(entryPath).isDirectory());
+        let leftoverFilePaths = dirPaths.reduce(
+            (filePaths, dirPath) => filePaths.concat(getFilePaths(dirPath)),
+            filePaths
+        );
+
+        return filePaths
+            .concat(leftoverFilePaths)
+            .filter(isUnique);
+    }
+
+    function getFileContent(filePath) {
+        return fs.readFileSync(filePath).toString();
+    }
+
+    function lintCss(fileModules) {
+        console.log();
+    }
+
+    Linter.lint = function (dirPath) {
+        let filePaths = getFilePaths(dirPath);
+        let fileModules = filePaths.map(filePath => {
+            return {
+                filePath: filePath,
+                rules: getFileContent(filePath)
+            };
+        });
+
+        lintCss(fileModules);
     };
 
     return Linter;
